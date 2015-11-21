1837a431e236ff6b7de55611f1f1e7ca51f3bae1
Nao Minami <south37777@gmail.com>
2015-11-21 19:48:20 +0900

    Add recipe for getting llvm code

--- cookbooks/development/recipes/default.rb
+++ cookbooks/development/recipes/default.rb
@@ -30,3 +30,35 @@ end
 package 'texinfo' do
   action :install
 end
+
+directory "~/llvm" do
+  owner "vagrant"
+  group "vagrant"
+  recursive true
+  mode 0755
+  action :create
+end
+
+remote_file "/tmp/llvm-3.7.0.src.tar.xz" do
+  source "http://llvm.org/releases/3.7.0/llvm-3.7.0.src.tar.xz"
+end
+
+remote_file "/tmp/cfe-3.7.0.src.tar.xz" do
+  source "http://llvm.org/releases/3.7.0/cfe-3.7.0.src.tar.xz"
+end
+
+remote_file "/tmp/compiler-rt-3.7.0.src.tar.xz" do
+  source "http://llvm.org/releases/3.7.0/compiler-rt-3.7.0.src.tar.xz"
+end
+
+script "install_wordpress" do
+  interpreter "bash"
+  user        "vagrant"
+  code <<-EOL
+    tar Jxf /tmp/llvm-3.7.0.src.tar.xz -C ~/llvm
+    mkdir ~/llvm/llvm-3.7.0.src/tools/clang
+    tar Jxf /tmp/cfe-3.7.0.src.tar.xz -C ~/llvm/llvm-3.7.0.src/tools/clang
+    mkdir ~/llvm/llvm-3.7.0.src/projects/compiler-rt
+    tar Jxf /tmp/compiler-rt-3.7.0.src.tar.xz -C ~/llvm/llvm-3.7.0.src/projects/compiler-rt
+  EOL
+end
